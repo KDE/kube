3dbb8aabc6576b1ff2c4b371822d555e2bb4f666
Michael Bohlender <michael.bohlender@kdemail.net>
2015-11-18 14:37:05 +0100

    no hard code filepath anymore

--- applications/kmail-quick/main.cpp
+++ applications/kmail-quick/main.cpp
@@ -1,9 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QStandardPaths>
+
+#include <KPackage/Package>
+#include <KPackage/PackageLoader>
+#include <KPackage/PackageStructure>
+
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine(QUrl::fromLocalFile("/home/chrigi/kdebuild/akonadinext/source/kontact-quick/applications/kmail-quick/package/contents/ui/main.qml"));
+    auto mainFile = QStandardPaths::locate(QStandardPaths::GenericDataLocation, "kpackage/genericqml/org.kde.pim.kmail-quick/contents/ui/main.qml", QStandardPaths::LocateFile);
+    QQmlApplicationEngine engine(QUrl::fromLocalFile(mainFile));
     return app.exec();
 }
