58f9f288fc5b3a7edf42e4dc997fe93418977249
Michael Bohlender <michael.bohlender@kdemail.net>
2015-12-07 00:17:51 +0100

    add folderlistmodel

---
+++ framework/mail/folderlistmodel.cpp
@@ -0,0 +1,42 @@
+#include "folderlistmodel.h"
+#include <akonadi2common/clientapi.h>
+
+FolderListModel::FolderListModel(QObject *parent) : QIdentityProxyModel()
+{
+    Akonadi2::Query query;
+    query.syncOnDemand = false;
+    query.processAll = false;
+    query.liveQuery = true;
+    QList<QByteArray> requestedProperties;
+    requestedProperties << "name" << "icon";
+    query.requestedProperties = requestedProperties.toSet();
+    //TODO
+    //mModel = Akonadi2::Store::loadModel<Akonadi2::ApplicationDomain::Mail>(query);
+    //setSourceModel(mModel.data());
+}
+
+FolderListModel::~FolderListModel()
+{
+
+}
+
+QHash< int, QByteArray > FolderListModel::roleNames() const
+{
+    QHash<int, QByteArray> roles;
+
+    roles[Name] = "name";
+    roles[Icon] = "icon";
+
+    return roles;
+}
+
+QVariant FolderListModel::data(const QModelIndex &idx, int role) const
+{
+    switch (role) {
+        case Name:
+            return mapToSource(idx).data(Qt::DisplayRole).toString();
+	case Icon:
+	    return mapToSource(idx).data(Qt::DisplayRole).toString();
+    }
+    return QIdentityProxyModel::data(idx, role);
+}
---
+++ framework/mail/folderlistmodel.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QObject>
+#include <QIdentityProxyModel>
+#include <QSharedPointer>
+#include <QStringList>
+
+class FolderListModel : public QIdentityProxyModel
+{
+    Q_OBJECT
+
+public:
+    FolderListModel(QObject *parent = Q_NULLPTR);
+    ~FolderListModel();
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+    enum Roles {
+        Name  = Qt::UserRole + 1,
+	Icon
+    };
+
+    QHash<int, QByteArray> roleNames() const;
+
+private:
+    QSharedPointer<QAbstractItemModel> mModel;
+};
