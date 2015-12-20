085a2f27086fc22ea984d7c57587d4d9de52c1c5
Michael Bohlender <michael.bohlender@kdemail.net>
2015-12-20 18:00:40 +0100

    register resources controller and listmodel as types in settignsplugin

--- framework/settings/settingsplugin.cpp
+++ framework/settings/settingsplugin.cpp
@@ -1,5 +1,7 @@
 #include "settingsplugin.h"
 
+#include "resourcescontroller.h"
+#include "resourcelistmodel.h"
 #include "maildir_resource.h"
 
 #include <QtQml>
@@ -8,6 +10,7 @@ void SettingsPlugin::registerTypes (const char *uri)
 {
     Q_ASSERT(uri == QLatin1String("org.kde.akonadi2.settings"));
 
-
+    qmlRegisterType<ResourceListModel>();
+    qmlRegisterType<ResourcesController>(uri, 1, 0, "Resources");
     qmlRegisterType<MaildirResouceController>(uri, 1, 0, "Maildir");
 }
