7a2f4ff0a4589fba2158f7cdbcd63027e25ed43e
Michael Bohlender <michael.bohlender@kdemail.net>
2015-12-20 01:13:05 +0100

    setter and getter for maildir name and folderurl

--- framework/settings/maildir_resource.cpp
+++ framework/settings/maildir_resource.cpp
@@ -4,3 +4,29 @@ MaildirResouceController::MaildirResouceController(QObject *parent) : QObject(pa
 {
 
 }
+
+QString MaildirResouceController::name() const
+{
+    return m_name;
+}
+
+void MaildirResouceController::setName(const QString &name)
+{
+    if(m_name != name) {
+        m_name = name;
+        emit nameChanged();
+    }
+}
+
+QUrl MaildirResouceController::folderUrl() const
+{
+    return m_folderUrl;
+}
+
+void MaildirResouceController::setFolderUrl(const QUrl &url)
+{
+    if(m_folderUrl != url) {
+        m_folderUrl = url;
+        emit folderUrlChanged();
+    }
+}
--- framework/settings/maildir_resource.h
+++ framework/settings/maildir_resource.h
@@ -7,12 +7,23 @@
 class MaildirResouceController : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY (QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY (QUrl fodlerUrl READ folderUrl WRITE setFolderUrl NOTIFY folderUrlChanged);
 
 public:
     explicit MaildirResouceController(QObject *parent = Q_NULLPTR);
 
+    QString name() const;
+    void setName(const QString &name);
+
+    QUrl folderUrl() const;
+    void setFolderUrl(const QUrl &url);
+
+signals:
+    void nameChanged();
+    void folderUrlChanged();
 
 private:
     QString m_name;
-    QUrl m_folderLocation;
+    QUrl m_folderUrl;
 };
