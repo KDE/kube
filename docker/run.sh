7aff8815c471035f8d73d4c769358dc0b2f5cf6a
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-25 10:23:13 +0200

    Fixed run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 SOURCEDIR=~/kdebuild/kube/source
 BUILDDIR=~/kdebuild/kube/build
 INSTALLDIR=~/kdebuild/kube/install
-docker run --rm -ti --privileged -u developer --security-opt seccomp:unconfined -v /tmp/.docker.xauth:/tmp/.docker.xauth -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/dri/card0:/dev/dri/card0 -e DISPLAY=:0 -e XAUTHORITY=/tmp/.docker.xauth -v $SOURCEDIR:/src -v $BUILDDIR:/build -v $INSTALLDIR:/install -w /build/ kubedev -c 'bash'
+docker run --rm -ti --privileged -u developer --security-opt seccomp:unconfined -v /tmp/.docker.xauth:/tmp/.docker.xauth -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/dri/card0:/dev/dri/card0 -e DISPLAY=:0 -e XAUTHORITY=/tmp/.docker.xauth -v $SOURCEDIR:/src -v $BUILDDIR:/build -v $INSTALLDIR:/install -w /build/ kubedev '/bin/bash'
