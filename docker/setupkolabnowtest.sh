71f5bbc1721eed453d3b0da9cdbb5f86ad11c28f
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-07-13 10:39:36 +0200

    The base url is enough

--- docker/setupkolabnowtest.sh
+++ docker/setupkolabnowtest.sh
@@ -1,6 +1,6 @@
 sinksh create account type kolabnow identifier kolabnowAccount name KolabnowAccount
 sinksh create resource type sink.imap identifier kolabnowImap account kolabnowAccount server imaps://imap.kolabnow.com:993 username test1@kolab.org
 sinksh create resource type sink.mailtransport identifier kolabnowSmtp account kolabnowAccount server smtps://smtp.kolabnow.com:587 username test1@kolab.org
-sinksh create resource type sink.carddav  identifier kolabnowCarddav account kolabnowAccount server https://apps.kolabnow.com/addressbooks/test1%40kolab.org  username test1@kolab.org
-sinksh create resource type sink.caldav  identifier kolabnowCaldav account kolabnowAccount server https://apps.kolabnow.com  username test1@kolab.org
+sinksh create resource type sink.carddav  identifier kolabnowCarddav account kolabnowAccount server https://apps.kolabnow.com username test1@kolab.org
+sinksh create resource type sink.caldav  identifier kolabnowCaldav account kolabnowAccount server https://apps.kolabnow.com username test1@kolab.org
 sinksh create identity name "John Doe" address test1@kolab.org account kolabnowAccount
