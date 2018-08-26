86355c650d373ad0033b2d6ae18d16f6f1644634
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-08-26 15:46:37 +0200

    Google testaccount setupscripts

---
+++ docker/setupgoogletest.sh
@@ -0,0 +1,7 @@
+#The account password is Welcome2Kube
+sinksh create account type google identifier googleAccount name Google
+sinksh create resource type sink.imap identifier googleImap account googleAccount server imaps://imap.gmail.com:993 username kubeprojecttest@gmail.com
+sinksh create resource type sink.mailtransport identifier googleSmtp account googleAccount server smtps://smtp.gmail.com:587 username kubeprojecttest@gmail.com
+sinksh create resource type sink.carddav  identifier googleCarddav account googleAccount server https://www.googleapis.com/carddav/v1/principals/kubeprojecttest@gmail.com/ username kubeprojecttest@gmail.com
+sinksh create resource type sink.caldav  identifier googleCaldav account googleAccount server https://www.google.com/calendar/dav/kubeprojecttest@gmail.com/ username kubeprojecttest@gmail.com
+sinksh create identity name "John Doe" address kubeprojecttest@gmail.com account googleAccount
