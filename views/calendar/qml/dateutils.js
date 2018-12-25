1ca1c15e710e02c20491f984eeace5e4a7399ac5
Christian Mollekopf <mollekopf@kolabsys.com>
2018-12-25 11:32:35 +0100

    Fixed typo

--- views/calendar/qml/dateutils.js
+++ views/calendar/qml/dateutils.js
@@ -17,7 +17,7 @@ function getWeek(date, dowOffset) {
         weeknum = Math.floor((daynum+day-1)/7) + 1;
         if(weeknum > 52) {
             var nYear = new Date(date.getFullYear() + 1,0,1);
-            nday = nYear.getDay() - dowOffset;
+            var nday = nYear.getDay() - dowOffset;
             nday = nday >= 0 ? nday : nday + 7;
             /*if the next year starts before the middle of
             the week, it is week #1 of that year*/
