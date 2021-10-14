6789ebf40e347166b11fcda6031b669615e97ea5
Christian Mollekopf <mollekopf@kolabsys.com>
2021-10-14 22:01:29 +0200

    InboundModel testing

--- framework/src/tests/inboundmodeltest.cpp
+++ framework/src/tests/inboundmodeltest.cpp
@@ -7,6 +7,7 @@
 #include <sink/resourcecontrol.h>
 #include <KCalCore/Event>
 #include <KCalCore/ICalFormat>
+#include "mailtemplates.h"
 
 #include "inboundmodel.h"
 
@@ -26,14 +27,14 @@ private slots:
 
         using namespace Sink::ApplicationDomain;
         auto account = ApplicationDomainType::createEntity<SinkAccount>();
-        Sink::Store::create(account).exec().waitForFinished();
+        VERIFYEXEC(Sink::Store::create(account));
 
         auto resource = Sink::ApplicationDomain::DummyResource::create(account.identifier());
-        Sink::Store::create(resource).exec().waitForFinished();
+        VERIFYEXEC(Sink::Store::create(resource));
 
         auto calendar1 = ApplicationDomainType::createEntity<Calendar>(resource.identifier());
         calendar1.setEnabled(true);
-        Sink::Store::create(calendar1).exec().waitForFinished();
+        VERIFYEXEC(Sink::Store::create(calendar1));
 
         const QDateTime start{{2018, 04, 17}, {6, 0, 0}};
         {
@@ -47,7 +48,7 @@ private slots:
             calcoreEvent->setAllDay(false);
             event1.setIcal(KCalCore::ICalFormat().toICalString(calcoreEvent).toUtf8());
             event1.setCalendar(calendar1);
-            Sink::Store::create(event1).exec().waitForFinished();
+            VERIFYEXEC(Sink::Store::create(event1));
         }
         {
             auto event2 = ApplicationDomainType::createEntity<Event>(resource.identifier());
@@ -61,10 +62,10 @@ private slots:
             calcoreEvent->recurrence()->setDaily(2);
             event2.setIcal(KCalCore::ICalFormat().toICalString(calcoreEvent).toUtf8());
             event2.setCalendar(calendar1);
-            Sink::Store::create(event2).exec().waitForFinished();
+            VERIFYEXEC(Sink::Store::create(event2));
         }
 
-        Sink::ResourceControl::flushMessageQueue(resource.identifier()).exec().waitForFinished();
+        VERIFYEXEC(Sink::ResourceControl::flushMessageQueue(resource.identifier()));
 
         InboundModel model;
 
@@ -105,8 +106,8 @@ private slots:
             QSignalSpy dataChangedSpy(&model, SIGNAL(dataChanged(const QModelIndex &, const QModelIndex &, const QVector<int> &)));
             QSignalSpy initialItemsLoadedSpy(&model, &InboundModel::initialItemsLoaded);
 
-            Sink::Store::modify(*event).exec().waitForFinished();
-            Sink::ResourceControl::flushMessageQueue(resource.identifier()).exec().waitForFinished();
+            VERIFYEXEC(Sink::Store::modify(*event));
+            VERIFYEXEC(Sink::ResourceControl::flushMessageQueue(resource.identifier()));
 
             QTest::qWait(200);
             QCOMPARE(resetSpy.count(), 0);
@@ -143,6 +144,75 @@ private slots:
             QCOMPARE(rowsInsertedSpy.count(), 0);
         }
     }
+
+    void testMailInbox()
+    {
+        using namespace Sink::ApplicationDomain;
+        auto account = ApplicationDomainType::createEntity<SinkAccount>();
+        VERIFYEXEC(Sink::Store::create(account));
+
+        auto resource = Sink::ApplicationDomain::DummyResource::create(account.identifier());
+        VERIFYEXEC(Sink::Store::create(resource));
+
+        auto folder1 = ApplicationDomainType::createEntity<Folder>(resource.identifier());
+        VERIFYEXEC(Sink::Store::create(folder1));
+
+        auto folder2 = ApplicationDomainType::createEntity<Folder>(resource.identifier());
+        VERIFYEXEC(Sink::Store::create(folder2));
+
+
+        auto mail1 = ApplicationDomainType::createEntity<Mail>(resource.identifier());
+        mail1.setFolder(folder1);
+        KMime::Types::Mailbox from;
+        from.fromUnicodeString("from@example.org");
+        auto message = MailTemplates::createMessage({}, {"foo@test.com"}, {}, {}, from, "Subject", "Body", false, {}, {}, {});
+        mail1.setMimeMessage(message->encodedContent(true));
+        VERIFYEXEC(Sink::Store::create(mail1));
+
+        auto mail2 = ApplicationDomainType::createEntity<Mail>(resource.identifier());
+        mail2.setFolder(folder2);
+        VERIFYEXEC(Sink::Store::create(mail2));
+
+        VERIFYEXEC(Sink::ResourceControl::flushMessageQueue(resource.identifier()));
+
+
+        //Inbound mode
+        {
+            InboundModel model;
+            QSignalSpy initialItemsLoadedSpy(&model, &InboundModel::initialItemsLoaded);
+            model.setCurrentDate({});
+            model.configure(
+                {}, // QSet<QString> &_senderBlacklist,
+                {}, // QSet<QString> &_toBlacklist,
+                {}, // QString &_senderNameContainsFilter,
+                {}, // QMap<QString, QString> &_perFolderMimeMessageWhitelistFilter,
+                {}, // QList<QRegularExpression> &_messageFilter,
+                {}, // QList<QString> &_folderSpecialPurposeBlacklist,
+                {}  // QList<QString> &_folderNameBlacklist,
+            );
+
+            QTest::qWait(200);
+            QTRY_COMPARE(model.rowCount({}), 2);
+            // QCOMPARE(initialItemsLoadedSpy.count(), 1);
+        }
+
+        //Folder mode
+        {
+            InboundModel model;
+            QSignalSpy initialItemsLoadedSpy(&model, &InboundModel::initialItemsLoaded);
+            model.setFilter({{"folder", QVariant::fromValue(ApplicationDomainType::Ptr::create(folder1))}});
+
+            QTRY_COMPARE(initialItemsLoadedSpy.count(), 1);
+            QCOMPARE(model.rowCount({}), 1);
+            {
+                auto idx = model.index(0, 0, {});
+                auto mail = idx.data(model.roleNames().key("data")).toMap().value("mail").value<Mail::Ptr>();
+                QVERIFY(mail);
+                QVERIFY(!mail->getSubject().isEmpty());
+            }
+        }
+    }
+
 };
 
 QTEST_MAIN(InboundModelTest)
